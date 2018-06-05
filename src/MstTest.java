8359b332308c17037960a3375c3348c4982cda54
Lucas Andre Salvino 112210094 <lucasas@lcc2-06.lcc.ufcg.edu.br>
2018-06-05 10:38:28 -0300

    testes mst

---
+++ src/MstTest.java
@@ -0,0 +1,29 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import controller.Controller;
+import graph.Graph;
+
+public class MstTest {
+
+	@Test
+    public void testMst() {
+
+		Controller ctrl;
+		Graph g;
+        try {
+
+            ctrl = new Controller();
+            g = ctrl.readGraph("testReadGraph.txt");
+
+        } catch (Exception e) {
+            fail();
+        }
+
+        //o metodo Mst não existe e não pode ser inicializado
+    }
+	
+	
+
+}
